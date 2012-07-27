1a092410e0c57a08b02c8bbcf89a42364439ae87
shstruct <sh.struct.0@gmail.com>
2012-07-26 22:57:56 -0700

    Thursday 10:58 PM

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '990801a0b4c62315ed53d68e8901c5963c428dccd21ab3046880030c747195f4e52c225675eb1d8f4d953875f418286dd130f0df872c1db95accd7e649dd8d1c';
